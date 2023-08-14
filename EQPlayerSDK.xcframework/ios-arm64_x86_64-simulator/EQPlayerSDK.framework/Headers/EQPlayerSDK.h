134da6aeec7434015da2f4b05c2b2be6592f0b3a
Gabriel Chong <gabielchong@gmail.com>
2023-08-14 15:28:19 +0800

    FEAT: introduce framework

---
+++ EQPlayerSDK.xcframework/ios-arm64_x86_64-simulator/EQPlayerSDK.framework/Headers/EQPlayerSDK.h
@@ -0,0 +1,18 @@
+//
+//  EQPlayerSDK.h
+//  EQPlayerSDK
+//
+//  Created by GabrielChong on 2023/8/14.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EQPlayerSDK.
+FOUNDATION_EXPORT double EQPlayerSDKVersionNumber;
+
+//! Project version string for EQPlayerSDK.
+FOUNDATION_EXPORT const unsigned char EQPlayerSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EQPlayerSDK/PublicHeader.h>
+
+
